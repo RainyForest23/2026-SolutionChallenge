c57071795c4060c36bc0b754681e492ec8fd7c38
RainyForest23 <wrim0923@gmail.com>
2026-03-20 18:55:29 +0900

    feat: add bgutil PO token provider for YouTube bot bypass

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,5 +8,11 @@ python manage.py migrate --noinput
 # echo "[entrypoint] Collecting static files..."
 # python manage.py collectstatic --noinput
 
+# Start bgutil PO token provider in background
+echo "[entrypoint] Starting bgutil PO token provider..."
+(cd /opt/bgutil-server/server/node_modules && \
+    deno run --allow-env --allow-net --allow-ffi=. --allow-read=. ../src/main.ts) &
+sleep 3
+
 echo "[entrypoint] Starting: $*"
 exec "$@"
